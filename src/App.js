f642913505db23bd4c5d3ac667cd6903a60d525d
PoolCano <poolg.cano@gmail.com>
2022-08-17 18:02:38 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Proyecto Panadería - CanoMantilla
         </p>
         <a
           className="App-link"
