3e745f49187bde3562b4578c2066746ac5a8399b
PoolCano <poolg.cano@gmail.com>
2022-09-14 21:18:27 -0500

    Noveno commit

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom';
+import ItemCount from "../components/itemCount";
 
 const ItemDetail = ({data}) => {
+
+  const [iraCart,setIraCart] = useState(false);
+
+  const onAdd = (cantidad) =>{
+    setIraCart(true);
+
+}
   return (
     <div>
       <div>
         <img src={data.thumbnail} alt="" />
         <div>
             <h1>{data.title}</h1>
+            {
+              iraCart? <Link to='/cart'>Completar compra</Link>
+              :<ItemCount
+              initial={3}
+              stock={5}
+              onAdd={onAdd}
+              />
+            }
         </div>
       </div>
     </div>
--- src/container/itemListContainer.jsx
+++ src/container/itemListContainer.jsx
@@ -1,22 +1,15 @@
-import ItemCount from "../components/itemCount";
+
 import ItemList from "../components/itemList";
 
 
 const ItemListContainer = ({greeting,color}) => {
 
-    const onAdd = (cantidad) =>{
-        console.log(`Compraste ${cantidad} unidades`)
-
-    }
+    
 
     return (
         <span>
             <h1 style={{backgroundColor: color}}>{greeting}</h1>
-            <ItemCount
-            initial={3}
-            stock={5}
-            onAdd={onAdd}
-            />
+            
             <ItemList/>
 
         </span>
